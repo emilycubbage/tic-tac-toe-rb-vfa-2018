1f0491c743a1f7a0337152de9b1a90b9df2f26c8
Emily Cubbage <emilycubbage@gmail.com>
2018-06-23 01:53:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -71,6 +71,24 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  counter = 0
+  board.each do |board|
+    if board == "X" || board == "O"
+      counter +=1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0 
+    return "X"
+  elsif turn_count(board) % 2 != 0 
+    return "O"
+  end
+end
+
 def won?(board)
   
   won_check = 0
