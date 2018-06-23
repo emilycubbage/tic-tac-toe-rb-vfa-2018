413b7aef85b84c6f2e33be6cb96d4d5f9a8c01b9
Emily Cubbage <emilycubbage@gmail.com>
2018-06-23 01:46:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,7 +24,10 @@ def display_board(board)
     puts " #{board[6]} | #{board[7]} | #{board[8]} " 
 end 
 
-
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+  return index
+end
 
 def won?(board)
   
